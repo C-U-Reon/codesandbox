69971a89bec06e35d86e4abdb742b16026779859
C-U-Reon <78156015+C-U-Reon@users.noreply.github.com>
2021-05-19 22:51:29 +0900

    2021/05/09 Udemy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
-import "./styles.css";
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+//
+
+// const val4 = {
+//   name: "レオン",
+//   age: 24,
+// };
+
+// val4.name = "reon";
+// val4.addres = "Kyoto"
+// console.log(val4);
+
+// const val5 = ['dog', 'cat'];
+// val5[0] = 'bird';
+// val5.push('monkey');
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+//  const name = "れおん";
+//  const age = 24;
+
+//  /**従来の方法 */
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+
+// //テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+//従来の関数
+// function func1(str) {
+//   return str;
+// }
+
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("funk1です"));
+
+//アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(func3(10, 20));
